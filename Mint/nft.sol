3c776d409e8e7009f9b90fb76794fdf47d1650e9
Steve <steve@zawadi.co>
2022-06-08 12:54:08 +0300

    Update nft.sol

--- Mint/nft.sol
+++ Mint/nft.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
